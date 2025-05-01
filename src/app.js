6ea03c61702150fa11cabbd70c445f061c76703b
AKASH TIWARI <akash9194@gmail.com>
2025-05-01 18:32:36 +0530

    tinder for dev initial push

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+// request handler
+// app.use('/', (req, res) =>{
+//     res.send("Namaskar!!!")
+// })
+app.use('/hello', (req, res) =>{
+    res.send("Hello hello hello!!!")
+})
+app.use('/test',(req, res) =>{
+    res.send("Hello test form the server !!!")
+})
+app.listen(3000, ()=>{
+    console.log("server is successfully listening on 3000")
+})
