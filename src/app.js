1bd0f2e2143f8dc1b9b1a06327c723bfd57b6c7e
AKASH TIWARI <akash9194@gmail.com>
2025-05-02 14:31:37 +0530

    exploring get and post

--- src/app.js
+++ src/app.js
@@ -3,15 +3,27 @@ const express = require('express');
 const app = express();
 
 // request handler
-// app.use('/', (req, res) =>{
-//     res.send("Namaskar!!!")
-// })
+
+
+app.use('/hello/2', (req, res) =>{
+    res.send("22 Hello hello hello!!!")
+});
 app.use('/hello', (req, res) =>{
     res.send("Hello hello hello!!!")
-})
+});
 app.use('/test',(req, res) =>{
     res.send("Hello test form the server !!!")
-})
+});
+app.get('/user',(req, res) =>{
+    res.send({firstName:"akash", lastName: "tiwari"})
+});
+app.post('/user',(req, res) =>{
+    console.log("save data to the database")
+    res.send({firstName:"akash", lastName: "tiwari"} + " added")
+});
+app.use('/', (req, res) =>{
+    res.send("Namaskar!!!")
+});
 app.listen(3000, ()=>{
     console.log("server is successfully listening on 3000")
-})
+});
